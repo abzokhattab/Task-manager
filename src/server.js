cc631daef6d490bc23efcc594da201d5bc6e1456
Abzokhattab <abzokhattab@gmail.com>
2020-03-24 10:50:28 +0100

    server settings

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, './public')))
+
+// this * route is to serve project on different page routes except root `/`
+
+
+const port = process.env.PORT || 8080
+app.listen(port)
